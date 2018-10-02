0b0ff7f89cab1ce615520710cbbedf614dec819d
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-10-02 23:20:11 +0200

    adjust preprocessor to text processing unit

--- code/pipeline/preprocessor.py
+++ code/pipeline/preprocessor.py
@@ -3,9 +3,10 @@ import json
 import subprocess
 from typing import *
 import spacy
+from text_processing_unit import TextProcessingUnit
 
 
-class Preprocessor:
+class Preprocessor(TextProcessingUnit):
     """Class to preprocess a corpus.
 
     Handle tokenization, pos-tagging and lemmatization of a corpus.
@@ -27,20 +28,12 @@ class Preprocessor:
         self.path_in = path_in
         self.path_out = path_out
         self.encoding = encoding
-        self._max_files = max_files
         self._nlp = spacy.load(path_lang_model)
-        self._files_processed = 0
-        self._sents_processed = 0
-        self._fnames = [fname for fname in os.listdir(self.path_in)
-                    if os.path.isfile(os.path.join(self.path_in, fname))]
-        self._fnames.sort()
-        self._num_files = len(self._fnames)
-        self._upper_bound = 0
+        super().__init__(path_in, path_out, max_files)
 
-        if not os.path.isdir(self.path_out):
-            os.makedirs(self.path_out)
-
-    def _write_json(self, annotated_sents: List[List[Tuple[str, str, str, bool]]]):
+    def _write_json(self,
+                    annotated_sents: List[List[Tuple[str, str, str, bool]]]
+                    ) -> None:
         """Write annotated_sents to a file in json.
 
         Json is formatted as follows:
@@ -69,11 +62,6 @@ class Preprocessor:
         """
         print(10*'-'+' preprocessing corpus '+10*'-')
         print('Input taken from: {}'.format(self.path_in))
-        if self._max_files:
-            self._upper_bound = min(len(self._fnames), self._max_files)
-        else:
-            self._upper_bound = len(self._fnames)
-
         for i in range(self._upper_bound):
             self._files_processed += 1
             self._process_file(self._fnames[i])
@@ -108,44 +96,9 @@ class Preprocessor:
             cmd, stdout=subprocess.PIPE).communicate()[0].decode('utf8')
         self._num_sents = int(out.strip(' ').split(' ')[0])
 
-    def _update_cmd(self) -> None:
-        """Update the information on the command line."""
-        final_msg = False
-        if self._files_processed == self._upper_bound:
-            if self._sents_processed == self._num_sents:
-                msg = 'Processing: sentence {}, file {} of {}'
-                print(msg.format(self._sents_processed, self._files_processed,
-                                 self._num_files))
-                final_msg = True
-        if not final_msg:
-            msg = 'Processing: sentence {}, file {} of {}\r'
-            print(msg.format(self._sents_processed, self._files_processed,
-                            self._num_files) , end='\r')
-
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-s',
-        '--server',
-        help="indicate if local paths or server paths should be used",
-        action='store_true')
-    args = parser.parse_args()
-    with open('configs.json', 'r', encoding='utf8') as f:
-        configs = json.load(f)
-        if args.server:
-            configs_server_pp = configs['server']['preprocessing']
-            path_in = configs_server_pp['path_in']
-            path_out = configs_server_pp['path_out']
-            spacy_model = configs_server_pp['spacy_model']
-            encoding = configs_server_pp['encoding']
-        else:
-            configs_local_pp = configs['local']['preprocessing']
-            path_in = configs_local_pp['path_in']
-            path_out = configs_local_pp['path_out']
-            spacy_model = configs_local_pp['spacy_model']
-            encoding = configs_local_pp['encoding']
-
-    pp = Preprocessor(path_in, path_in, spacy_model, encoding)
+    from utility_functions import get_config
+    config = get_config('preprocessing')
+    pp = Preprocessor(**config)
     pp.process_corpus()
