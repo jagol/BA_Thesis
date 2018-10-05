922076ff04a350a32710a9d37261dda6531e3675
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-10-05 17:38:58 +0200

    update get_corpus_config

--- code/pipeline/utility_functions.py
+++ code/pipeline/utility_functions.py
@@ -1,14 +1,25 @@
 import json
 import argparse
+from typing import Tuple, Dict
 
-def get_config(unit):
+
+corpus_config = Dict[str, Dict[str, str]]
+
+
+def get_corpus_config(unit: str) -> Tuple[str, corpus_config]:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-s',
         '--server',
-        help="indicate if local paths or server paths should be used",
+        help='indicate if local paths or server paths should be used',
         action='store_true')
+    parser.add_argument(
+        '-c',
+        '--corpus',
+        help='indicate name of corpus to be processed: europarl; dblp;'
+    )
     args = parser.parse_args()
+
     with open('configs.json', 'r', encoding='utf8') as f:
         configs = json.load(f)
         if args.server:
@@ -16,4 +27,12 @@ def get_config(unit):
         else:
             location = 'local'
 
-    return configs[location][unit]
+        dblp = 'dblp'
+        euprl = 'europarl'
+
+        if args.corpus == dblp:
+            return dblp, configs[location][dblp][unit]
+        elif args.corpus == euprl:
+            return euprl, configs[location][euprl][unit]
+        else:
+            raise Exception('Error! Corpus not known.')
