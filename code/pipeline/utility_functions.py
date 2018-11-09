54856f1006efc9fc3196c1a4f69f81a03b9bf21d
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-11-09 12:15:43 +0100

    add get_docs and format_doc

--- code/pipeline/utility_functions.py
+++ code/pipeline/utility_functions.py
@@ -1,12 +1,72 @@
 import json
 import argparse
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List, Generator, Union
+
+# ----------------------------------------------------------------------
+# type definitions
+corpus_config_type = Dict[str, Dict[str, str]]
+doc_type = Union[List[str], List[List[str]], str]
+
+# ----------------------------------------------------------------------
 
 
-corpus_config = Dict[str, Dict[str, str]]
+def get_docs(fpath: str,
+             word_tokenized: bool = True,
+             sent_tokenized: bool = True
+             ) -> Generator[doc_type, None, None]:
+    """Yield each document of a corpus.
+
+    Load the corpus from a file. There is one line per sentence. Between
+    each document there is an additional newline. If tokenized is true,
+    yield each document as a list of strings. If not, yield each
+    document as a string.
+
+    Args:
+        fpath: path to corpus file
+        word_tokenized: indicate, if output should be word_tokenized
+        sent_tokenized: indicate, if output should be split into
+            sentences
+    Return:
+        a generator object of documents
+    """
+    with open(fpath, 'r', encoding='utf8') as f:
+        doc = []
+        for line in f:
+            if line == '\n':
+                yield format_doc(doc, word_tokenized, sent_tokenized)
+            else:
+                doc.append(line.strip('\n'))
 
 
-def get_corpus_config(unit: str) -> Tuple[str, corpus_config]:
+def format_doc(doc: List[str],
+               word_tokenized: bool,
+               sent_tokenized: bool
+               ) -> doc_type:
+    """Return a document in the required format.
+
+     Returns document as a list of tokens, a list of sentences, a list
+     of tokenized sentences or a string, depending on the input
+     configuration.
+
+     Args:
+         doc: A List of sentences. Each token in the sentence is
+            separated by a space.
+        word_tokenized: indicate, if output should be word-tokenized
+        sent_tokenized: indicate, if output should be split into
+            sentences
+     """
+    if word_tokenized and sent_tokenized:
+        return [sent.split(' ') for sent in doc]
+    elif word_tokenized:
+        doc_str = ' '.join(doc)
+        return [w for w in doc_str.split(' ')]
+    elif sent_tokenized:
+        return doc
+    else:
+        return ' '.join(doc)
+
+
+def get_corpus_config(unit: str) -> Tuple[str, corpus_config_type]:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-s',
