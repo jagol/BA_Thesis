23f1f0c8e8dbf58adb9efaed51d704a3a1e0611d
Janis Goldzycher <janisgoldzycher@gmail.com>
2018-11-15 09:17:23 +0100

    add frequency analysis class

---
+++ code/pipeline/frequency_analysis.py
@@ -0,0 +1,16 @@
+import time
+
+
+class FreqAnalyzer:
+
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def calc_tf(self):
+        pass
+
+    def calc_df(self):
+        pass
+
+    def calc_dl(self):
+        pass
