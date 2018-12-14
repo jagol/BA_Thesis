ff75a0b182984377d9985a7f8630aa14928c3c8d
Janis Goldzycher <janisgoldzycher@mailbox.org>
2018-12-14 16:19:17 +0100

    add methods to calc frequencies of lemmas and tokens

--- code/pipeline/frequency_analysis.py
+++ code/pipeline/frequency_analysis.py
@@ -16,14 +16,25 @@ class FreqAnalyzer:
             path: The path to the output directory.
         """
         self.path = path
+
+        self.path_token_idx_corpus = os.path.join(
+            path, 'processed_corpus/token_idx_corpus.txt')
+        self.path_token_terms_idxs = os.path.join(
+            path, 'processed_corpus/token_terms_idxs.txt')
+        self.path_tf_tokens = os.path.join(
+            path, 'frequencies/tf_tokens.json')
+        self.path_df_tokens = os.path.join(
+            path, 'frequencies/df_tokens.json')
+
         self.path_lemma_idx_corpus = os.path.join(
             path, 'processed_corpus/lemma_idx_corpus.txt')
-        self.path_terms_idxs = os.path.join(
+        self.path_lemma_terms_idxs = os.path.join(
             path, 'processed_corpus/lemma_terms_idxs.txt')
-        self.path_tf = os.path.join(
+        self.path_tf_lemmas = os.path.join(
             path, 'frequencies/tf_lemmas.json')
-        self.path_df = os.path.join(
+        self.path_df_lemmas = os.path.join(
             path, 'frequencies/df_lemmas.json')
+
         self.path_dl = os.path.join(
             path, 'frequencies/dl.json')
 
@@ -32,16 +43,24 @@ class FreqAnalyzer:
         self._docs_processed = 0
         # super().__init__(path, path, max_docs)
 
-    def calc_tf(self) -> None:
+    def calc_tf(self, level) -> None:
         """Calculate the term frequency of each term for each document.
 
         Create a file tf_lemmas.txt, which has one dictionary per line:
         {lemma_idx: tf}
         The dictionary of line 1 belongs to document 1 etc.
         """
-        term_idxs = self._load_term_idxs()
+        if level == 't':
+            term_idxs = self._load_term_idxs('t')
+            path_in = self.path_token_idx_corpus
+            path_out = self.path_tf_tokens
+        elif level == 'l':
+            term_idxs = self._load_term_idxs('l')
+            path_in = self.path_lemma_idx_corpus
+            path_out = self.path_tf_lemmas
+
         dicts = []
-        for doc in get_docs(self.path_lemma_idx_corpus):
+        for doc in get_docs(path_in):
             df = defaultdict(int)
             for sent in doc:
                 for lemma_idx in sent:
@@ -55,16 +74,26 @@ class FreqAnalyzer:
                 msg = '{} documents processed, writing to file...'
                 print(msg.format(self._docs_processed))
                 mode = self._get_write_mode()
-                with open(self.path_df, mode, encoding='utf8') as f:
+                with open(path_out, mode, encoding='utf8') as f:
                     for d in dicts:
                         f.write(json.dumps(d) + '\n')
                 dicts = []
 
         self._docs_processed = 0
 
-    def _load_term_idxs(self) -> Set[int]:
+    def _load_term_idxs(self, level: str) -> Set[int]:
+        """Load term indices from file.
+
+        Args:
+            level: 't' if tokens, 'l' if lemmas.
+        """
         term_idxs = set()
-        with open(self.path_terms_idxs, 'r', encoding='utf8') as f:
+        if level == 't':
+            path = self.path_token_terms_idxs
+        elif level == 'l':
+            path = self.path_lemma_terms_idxs
+
+        with open(path, 'r', encoding='utf8') as f:
             for line in f:
                 term_idxs.add(int(line.strip('\n')))
         return term_idxs
@@ -75,16 +104,26 @@ class FreqAnalyzer:
             return 'w'
         return 'a'
 
-    def calc_df(self) -> None:
+    def calc_df(self, level: str) -> None:
         """Calculate the document frequency for all terms.
 
         Do plus one smoothing to avoid zero division errors. Write
         output to 'frequency_analysis/df_lemmas.json' in form
         of a dict: {<term_id>: df}
+
+        Args:
+            level: 't' if tokens, 'l' if lemmas.
         """
-        term_idxs = self._load_term_idxs()
+        if level == 't':
+            path_df = self.path_df_tokens
+            path_idx_corpus = self.path_token_idx_corpus
+        elif level == 'l':
+            path_df = self.path_df_lemmas
+            path_idx_corpus = self.path_lemma_idx_corpus
+
+        term_idxs = self._load_term_idxs(level)
         df = defaultdict(int)
-        for doc in get_docs(self.path_lemma_idx_corpus):
+        for doc in get_docs(path_idx_corpus):
             for t in term_idxs:
                 if self._is_in_doc(t, doc):
                     df[t] += 1
@@ -94,8 +133,8 @@ class FreqAnalyzer:
 
         msg = '{} documents processed'
         print(msg.format(self._docs_processed))
-
-        with open(self.path_df, 'w', encoding='utf8') as f:
+        print(path_df)
+        with open(path_df, 'w', encoding='utf8') as f:
             json.dump(df, f)
 
         self._docs_processed = 0
@@ -145,8 +184,10 @@ class FreqAnalyzer:
 
 def main():
     fa = FreqAnalyzer('./output/dblp/')
-    fa.calc_tf()
-    fa.calc_df()
+    fa.calc_tf('t')
+    fa.calc_tf('l')
+    fa.calc_df('t')
+    fa.calc_df('l')
     fa.calc_dl()
 
 
