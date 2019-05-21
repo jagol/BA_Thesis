3eeb4269094d0522b6219ccb5b2d19b196c83b9e
Janis Goldzycher <janisgoldzycher@mailbox.org>
2019-05-21 16:26:19 +0200

    better integration of elmo embeddings

--- code/pipeline/document_embeddings.py
+++ code/pipeline/document_embeddings.py
@@ -19,6 +19,8 @@ class DocEmbedder:
             self.path_out, 'embeddings/embs_lemma_global_GloVe.vec')
         self.path_term_embs_token_glove = os.path.join(
             self.path_out, 'embeddings/embs_token_global_GloVe.vec')
+        self.path_term_embs_token_elmo = os.path.join(
+            self.path_out, 'embeddings/embs_token_ELMo_context.pickle')
         self.path_tfidf_tokens = os.path.join(
             path_out, 'frequencies/tfidf_tokens.json')
         self.path_tfidf_lemmas = os.path.join(
@@ -35,6 +37,13 @@ class DocEmbedder:
             path_out, 'embeddings/doc_embs_token_GloVe.pickle')
         self.path_doc_embs_lemma_glove = os.path.join(
             path_out, 'embeddings/doc_embs_lemma_GloVe.pickle')
+        self.path_doc_embs_token_elmo = os.path.join(
+            path_out, 'embeddings/doc_embs_token_ELMo.pickle')
+        self.path_idx_to_term = os.path.join(
+            self.path_out, 'indexing/idx_to_token.json')
+        with open(self.path_idx_to_term, 'r', encoding='utf8') as f:
+            tmp_dct = json.load(f)
+            self.idx_to_term = {int(k): v for k, v in tmp_dct.items()}
 
     def embed_docs(self):
         """Compute document embeddings.
@@ -43,7 +52,7 @@ class DocEmbedder:
             A Keyed Vector file mapping each doc-id to its embedding.
         """
         self.embed_token_docs()
-        self.embed_lemma_docs()
+        # self.embed_lemma_docs()
 
     def embed_token_docs(self):
         """Compute token document embeddings.
@@ -51,16 +60,21 @@ class DocEmbedder:
         Output:
             A Keyed Vector file mapping each doc-id to its embedding.
         """
-        print('Calculate token w2v embeddings...')
-        self._embed_docs(self.path_term_embs_token_w2v,
+        # print('Calculate token w2v embeddings...')
+        # self._embed_docs(self.path_term_embs_token_w2v,
+        #                  self.path_token_term_idxs,
+        #                  self.path_tfidf_tokens,
+        #                  self.path_doc_embs_token_w2v)
+        # print('Calculate token glove embeddings...')
+        # self._embed_docs(self.path_term_embs_token_glove,
+        #                  self.path_token_term_idxs,
+        #                  self.path_tfidf_tokens,
+        #                  self.path_doc_embs_token_glove)
+        print('Calculate token elmo embeddings...')
+        self._embed_docs(self.path_term_embs_token_elmo,
                          self.path_token_term_idxs,
                          self.path_tfidf_tokens,
-                         self.path_doc_embs_token_w2v)
-        print('Calculate token glove embeddings...')
-        self._embed_docs(self.path_term_embs_token_glove,
-                         self.path_token_term_idxs,
-                         self.path_tfidf_tokens,
-                         self.path_doc_embs_token_glove)
+                         self.path_doc_embs_token_elmo)
 
     def embed_lemma_docs(self):
         """Compute lemma document embeddings.
@@ -87,7 +101,8 @@ class DocEmbedder:
                     ) -> None:
         """Calculate Document embeddings."""
         term_idxs = self.load_term_idxs(path_term_idxs)
-        term_embs = Embeddings.load_term_embeddings(term_idxs, path_term_embs, term_ids_to_embs_global={})
+        term_embs = Embeddings.load_term_embeddings(
+            term_idxs, path_term_embs, {}, self.idx_to_term)
         with open(path_tfidf, 'r', encoding='utf8') as f:
             tfidf = json.load(f)
         doc_embeddings = {}
@@ -107,8 +122,8 @@ class DocEmbedder:
         with open(path_doc_embs, 'wb') as f:
             pickle.dump(doc_embeddings, f)
 
-
-    def load_term_idxs(self, path: str) -> Set[int]:
+    @staticmethod
+    def load_term_idxs(path: str) -> Set[int]:
         term_idxs = set()
         with open(path, 'r', encoding='utf8') as f:
             for line in f:
