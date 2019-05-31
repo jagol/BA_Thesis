102926b9c7a8954e984fb9b4468dc8e6a97b20e4
Janis Goldzycher <janisgoldzycher@mailbox.org>
2019-06-01 01:52:02 +0200

    efficient elmo computation

--- code/pipeline/document_embeddings.py
+++ code/pipeline/document_embeddings.py
@@ -2,8 +2,9 @@ import os
 import json
 import pickle
 from typing import *
-from numpy import mean
+import numpy as np
 from embeddings import Embeddings
+import pdb
 
 
 class DocEmbedder:
@@ -20,7 +21,7 @@ class DocEmbedder:
         self.path_term_embs_token_glove = os.path.join(
             self.path_out, 'embeddings/embs_token_global_GloVe.vec')
         self.path_term_embs_token_elmo = os.path.join(
-            self.path_out, 'embeddings/embs_token_ELMo_context.pickle')
+            self.path_out, 'embeddings/embs_token_ELMo_avg.txt')
         self.path_tfidf_tokens = os.path.join(
             path_out, 'frequencies/tfidf_tokens.json')
         self.path_tfidf_lemmas = os.path.join(
@@ -99,17 +100,33 @@ class DocEmbedder:
                     path_tfidf: str,
                     path_doc_embs: str
                     ) -> None:
-        """Calculate Document embeddings."""
+        """Calculate Document embeddings.
+
+        Args:
+            path_term_embs: Path to term embedding file.
+            path_term_idxs: Path to term index file.
+            path_tfidf: Path to term tfidf file.
+            path_doc_embs: Path to file, where documents embeddings
+                will be written.
+        """
+        print('Loading term indices...')
         term_idxs = self.load_term_idxs(path_term_idxs)
+        print('Loading embeddings...')
         term_embs = Embeddings.load_term_embeddings(
-            term_idxs, path_term_embs, {}, self.idx_to_term)
+            term_idxs, path_term_embs, self.idx_to_term)
+        del term_idxs
+        print('Loading tfidf scores...')
         with open(path_tfidf, 'r', encoding='utf8') as f:
             tfidf = json.load(f)
-        doc_embeddings = {}
 
-        for doc_id in tfidf:
+        print('Computing document embeddings...')
+        i = 0
+        num_docs = len(tfidf)
+        doc_embeddings = np.empty((num_docs, 1024))
+        doc_ids = [i for i in range(num_docs)]
+        for doc_id in doc_ids:
             doc_emb = []
-            tfidf_doc = tfidf[doc_id]
+            tfidf_doc = tfidf[str(doc_id)]
             if len(tfidf_doc) == 0:
                 continue
             for term_id in tfidf_doc:
@@ -117,10 +134,20 @@ class DocEmbedder:
                 tfidf_term = tfidf_doc[term_id]
                 term_emb_weighted = tfidf_term * term_emb
                 doc_emb.append(term_emb_weighted)
-            doc_embeddings[int(doc_id)] = mean(doc_emb, axis=0)
+            doc_embeddings[doc_id] = np.mean(doc_emb, axis=0)
+            if i % 100000 == 0 and i != 0:
+                print('Processed documents: {} out of {}.'.format(i, num_docs))
+            i += 1
+        del tfidf
+        print('Finished computing document embeddings...')
 
-        with open(path_doc_embs, 'wb') as f:
-            pickle.dump(doc_embeddings, f)
+        msg = '{} of {} embeddings written to file.'
+        with open(path_doc_embs, 'w', encoding='utf8') as f:
+            for doc_id, emb in enumerate(doc_embeddings):
+                line = ','.join([str(f) for f in emb])
+                f.write(line+'\n')
+                if i % 100000 == 0:
+                    print(msg.format(i, num_docs))
 
     @staticmethod
     def load_term_idxs(path: str) -> Set[int]:
